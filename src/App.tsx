928a95794f9f3aad81acc8d2a75bcaaaeaff5521
devofficer <sompaqueen47@gmail.com>
2023-04-04 18:06:33 -0700

    feat: integrate unity webgl build

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,12 @@
-import { useEffect, useRef } from 'react';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-
-import { WorldEvent } from '@/engine/enums/WorldEvent';
-import { World } from '@/engine/world';
-import Loading from './pages/loading';
-import LINKS from './utils/constants/links';
+import { Unity, useUnityContext } from 'react-unity-webgl';
 
 export default function App() {
-  const initialized = useRef(false);
-  const canvas = useRef();
-  const navigate = useNavigate();
+  const { unityProvider } = useUnityContext({
+    loaderUrl: 'build/tws.loader.js',
+    dataUrl: 'build/tws.data.unityweb',
+    frameworkUrl: 'build/tws.framework.js.unityweb',
+    codeUrl: 'build/tws.wasm.unityweb',
+  });
 
-  const handleWorldEvent = (code: WorldEvent) => {
-    if (code === WorldEvent.JoinedRoom) {
-      setTimeout(() => navigate(LINKS.game), 500);
-    }
-  };
-
-  useEffect(() => {
-    if (!initialized.current) {
-      const world = new World(canvas.current, '/assets/models/world.glb');
-      world.onEvent = handleWorldEvent;
-      initialized.current = true;
-    }
-  }, []);
-
-  return (
-    <div className="w-100 h-100 relative">
-      <div className="absolute top-0 left-0 z-10">
-        <Routes>
-          <Route path={LINKS.loading} Component={Loading} />
-          <Route path={LINKS.game} element={null} />
-        </Routes>
-      </div>
-      <canvas ref={canvas} className="absolute top-0 left-0 z-0" />
-    </div>
-  );
+  return <Unity unityProvider={unityProvider} className="h-screen w-screen" />;
 }
